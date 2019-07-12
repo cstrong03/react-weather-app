26533d930cf37d0e88ef2eb7b66cca2103e81b35
Charisse Strong <msstrong92@gmail.com>
2019-07-11 22:25:34 -0400

    code up might need new api key

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const Api_Key = process.env.REACT_APP_WEATHER_API_KEYS;
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state ={
+      ZipCode:'10457',
+      name: '',
+      temperature: '',
+      description: '',
+      tempMax: '',
+      tempMin: '',
+    }
+
+  }
+
+  handleWeatherInput=(event)=>{
+    event.preventDefault()
+    this.setState({
+      ZipCode:event.target.zipCode.value
+    })
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${this.state.ZipCode},us&appid=${Api_Key}&units=imperial`
+
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>{
+      console.log(data)
+      this.setState({
+        name: data.name,
+        temperature: data.main.temp,
+        description: data.weather[0].description,
+        tempMax: data.main.temp_max,
+        tempMin: data.main.temp_min
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <div>
+        <h1>Weather App</h1>
+        <h3>CurrentCity: {this.state.name}</h3>
+        <h3>Weather: {this.state.temperature}</h3>
+        <h3>description: {this.state.description}</h3>
+      </div>
+      <form onSubmit={this.handleWeatherInput}>
+        <input type='text' placeholder='Enter Your ZipCode' name='zipCode' />
+        <input type='submit' value='submit' />
+
+      </form>
+      </div>
+    );
+  }
 }
 
 export default App;
